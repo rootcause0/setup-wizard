203af69c8f8687facc455419eae2d4b6c644471b
Your Name <you@example.com>
2021-09-22 21:28:57 +0300

    Initial Commit

---
+++ src/Router.js
@@ -0,0 +1,6 @@
+import HelloWorld from './components/HelloWorld'
+const routes = [
+  { path: '/deneme', component: HelloWorld}, 
+]
+
+export default routes
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from './Router'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+// Module Router,Must inject via Vue.use to the our Vue.App
+Vue.use(VueRouter)
+var router = new VueRouter({
+  routes: routes,
+  mode: 'history'
+})
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router: router,
+  render: h => h(App)
+})
